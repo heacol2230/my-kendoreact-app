0c31fa97f661fd3cab8318f3dba2d456017cdf99
heacol2230 <heacol2230@students.ecpi.edu>
2024-04-28 01:35:59 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import kendoka from './kendoka.svg';
 import './App.scss';
+import { Calendar } from "@progress/kendo-react-dateinputs";
 
+// eslint-disable-next-line no-unused-vars
 function App() {
-  const handleClick = React.useCallback(() => {
-    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={kendoka} className="App-logo" alt="kendoka" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button
-          themeColor={'primary'}
-          size={"large"}
-          onClick={handleClick}
-        >
-          Learn KendoReact
-        </Button>
-      </header>
+      <h1>Hello KendoReact!</h1>
+      <Calendar />
     </div>
   );
 }
 
-export default App;
+export default App; 
